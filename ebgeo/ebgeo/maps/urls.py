8ea2e18b73b297be471a5cc41b5b969c269dbd04
Everyblock User <everyblock@centos.localdomain>
2010-01-31 13:56:30 -0500

    views.export_pdf does not exists

--- ebgeo/ebgeo/maps/urls.py
+++ ebgeo/ebgeo/maps/urls.py
@@ -5,6 +5,6 @@ from ebgeo.maps.tilecache_service import request_pat as tile_request_pat
 urlpatterns = patterns('',
     (r'^tile%s' % tile_request_pat, views.get_tile),
     (r'^locator/(?P<version>\d+\.\d+)/(?P<city>\w{1,32})\.(?P<extension>(?:png|jpg|gif))$', views.locator_map),
-    (r'^browser/export_pdf/$', views.export_pdf),
+    #(r'^browser/export_pdf/$', views.export_pdf),
     (r'^marker_(?P<radius>\d{1,2})\.png$', views.get_marker),
 )
