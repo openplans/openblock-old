1ccafc697e1d31053abddb12137505edf5ab4cce
Paul Winkler <slinkp@gmail.com>
2010-08-24 14:00:39 -0400

    Renamed sql function files so they get run automatically on manage.py syncdb; also applied a workaround for brokenness in django's handling of multiline raw sql statements.

--- ebpub/ebpub/db/sql/newsitemlocation.sql
+++ ebpub/ebpub/db/sql/newsitemlocation.sql
@@ -1,8 +1,11 @@
 -- Trigger that updates db_newsitemlocation whenever the location is changed in
 -- db_newsitem.
+
+-- Ending statements with "--" as workaround described at http://code.djangoproject.com/ticket/3214.
+
 CREATE OR REPLACE FUNCTION update_newsitem_location() RETURNS TRIGGER AS $location_updater$
     DECLARE
-        loc_id integer;
+        loc_id integer; --
     BEGIN
         -- The following IF statement is overly verbose because of NULL
         -- equality. We simply want to do 'NEW.location != OLD.location', but
@@ -16,30 +19,30 @@ CREATE OR REPLACE FUNCTION update_newsitem_location() RETURNS TRIGGER AS $locati
             -- http://archive.netbsd.se/?ml=pgsql-sql&a=2005-09&t=1337824
             IF ((NEW.location IS NOT NULL AND OLD.location IS NOT NULL AND NEW.location != OLD.location) OR (NEW.location IS NULL AND OLD.location IS NOT NULL) OR (NEW.location IS NOT NULL AND OLD.location IS NULL)) THEN
                 IF (OLD.location IS NOT NULL) THEN
-                    DELETE FROM db_newsitemlocation WHERE news_item_id = OLD.id;
-                END IF;
+                    DELETE FROM db_newsitemlocation WHERE news_item_id = OLD.id; --
+                END IF; --
                 IF (NEW.location IS NOT NULL) THEN
                     IF (GeometryType(NEW.location) = 'GEOMETRYCOLLECTION') THEN
                         FOR i IN 1..ST_NumGeometries(NEW.location) LOOP
                                 FOR loc_id IN SELECT id FROM db_location WHERE intersects(db_location.location, ST_GeometryN(NEW.location, i)) LOOP
-                                    PERFORM * FROM db_newsitemlocation WHERE news_item_id = NEW.id AND location_id = loc_id;
+                                    PERFORM * FROM db_newsitemlocation WHERE news_item_id = NEW.id AND location_id = loc_id; --
                                     IF NOT FOUND THEN
-                                        INSERT INTO db_newsitemlocation (news_item_id, location_id) VALUES (NEW.id, loc_id);
-                                    END IF;
-                                END LOOP;
-                        END LOOP;
+                                        INSERT INTO db_newsitemlocation (news_item_id, location_id) VALUES (NEW.id, loc_id); --
+                                    END IF; --
+                                END LOOP; --
+                        END LOOP; --
                     ELSE
                         INSERT INTO db_newsitemlocation (news_item_id, location_id)
-                        SELECT NEW.id, id FROM db_location WHERE intersects(db_location.location, NEW.location);
-                    END IF;
-                END IF;
-            END IF;
+                        SELECT NEW.id, id FROM db_location WHERE intersects(db_location.location, NEW.location); --
+                    END IF; --
+                END IF; --
+            END IF; --
             -- Update "Unknown" locations
             INSERT INTO db_newsitemlocation (news_item_id, location_id)
             SELECT NEW.id AS news_item_id, (SELECT id FROM db_location WHERE location_type_id=db_locationtype.id AND slug='unknown') AS location_id
             FROM db_locationtype
             WHERE NOT EXISTS (SELECT 1 FROM db_newsitemlocation WHERE news_item_id=NEW.id)
-            AND db_locationtype.is_significant = true;
+            AND db_locationtype.is_significant = true; --
         ELSIF (TG_OP = 'INSERT') THEN
             -- See the above comment for why this statement isn't combined into
             -- the previous one.
@@ -47,34 +50,34 @@ CREATE OR REPLACE FUNCTION update_newsitem_location() RETURNS TRIGGER AS $locati
                 IF (GeometryType(NEW.location) = 'GEOMETRYCOLLECTION') THEN
                     FOR i IN 1..ST_NumGeometries(NEW.location) LOOP
                             FOR loc_id IN SELECT id FROM db_location WHERE intersects(db_location.location, ST_GeometryN(NEW.location, i)) LOOP
-                                PERFORM * FROM db_newsitemlocation WHERE news_item_id = NEW.id AND location_id = loc_id;
+                                PERFORM * FROM db_newsitemlocation WHERE news_item_id = NEW.id AND location_id = loc_id; --
                                 IF NOT FOUND THEN
-                                    INSERT INTO db_newsitemlocation (news_item_id, location_id) VALUES (NEW.id, loc_id);
-                                END IF;
-                            END LOOP;
-                    END LOOP;
+                                    INSERT INTO db_newsitemlocation (news_item_id, location_id) VALUES (NEW.id, loc_id); --
+                                END IF; --
+                            END LOOP; --
+                    END LOOP; --
                 ELSE
                     INSERT INTO db_newsitemlocation (news_item_id, location_id)
-                    SELECT NEW.id, id FROM db_location WHERE intersects(db_location.location, NEW.location);
-                END IF;
-            END IF;
+                    SELECT NEW.id, id FROM db_location WHERE intersects(db_location.location, NEW.location); --
+                END IF; --
+            END IF; --
             -- Update "Unknown" locations
             INSERT INTO db_newsitemlocation (news_item_id, location_id)
             SELECT NEW.id AS news_item_id, (SELECT id FROM db_location WHERE location_type_id=db_locationtype.id AND slug='unknown') AS location_id
             FROM db_locationtype
             WHERE NOT EXISTS (SELECT 1 FROM db_newsitemlocation WHERE news_item_id=NEW.id)
-            AND db_locationtype.is_significant = true;
+            AND db_locationtype.is_significant = true; --
         ELSIF (TG_OP = 'DELETE') THEN
-            DELETE FROM db_newsitemlocation WHERE news_item_id = OLD.id;
-            RETURN OLD;
-        END IF;
-        RETURN NEW;
-    END;
-$location_updater$ LANGUAGE plpgsql;
+            DELETE FROM db_newsitemlocation WHERE news_item_id = OLD.id; --
+            RETURN OLD; --
+        END IF; --
+        RETURN NEW; --
+    END; --
+$location_updater$ LANGUAGE plpgsql; --
 
 CREATE TRIGGER location_updater
 BEFORE INSERT OR UPDATE OR DELETE ON db_newsitem
-    FOR EACH ROW EXECUTE PROCEDURE update_newsitem_location();
+    FOR EACH ROW EXECUTE PROCEDURE update_newsitem_location(); --
 
 -- To delete:
 -- DROP TRIGGER location_updater ON db_newsitem;
