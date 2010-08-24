9836a9962314610bb2215f5894813d84329c675a
Paul Winkler <slinkp@gmail.com>
2010-08-24 14:02:20 -0400

    remove comment about sql files that are now automatically installed

--- obdemo/obdemo/views.py
+++ obdemo/obdemo/views.py
@@ -27,28 +27,7 @@ def newsitems_geojson(request):
         search_buffer = make_search_buffer(place.location.centroid, block_radius)
         newsitem_qs = NewsItem.objects.filter(location__bboverlaps=search_buffer)
     else:
-        # I'm puzzled about when/how NewsItemLocations get created,
-        # they're clearly just a simple join table but
-        # I can't find anything that would populate when news items
-        # are created,
-        # except maybe a trigger function in ebpub/ebpub/db/sql/newsitemlocation_functions.sql
-        # which AFAICT is not referred to by anything, so I'm not sure if I'm
-        # supposed to have installed that function or what.
-        # 
-        # I *do* see code in ebpub/db/bin/*py to populate that table
-        # from existing newsitems when locations are added, but not
-        # when newsitems are added AFAICT.
-        #
-        # So I'm hacking around it by ORing the query together with a
-        # test for news items that are directly associated with that
-        # Location. Why existing ebpub code doesn't do that, I do not
-        # know.
-        # newsitem_qs = NewsItem.objects.filter(
-        #     Q(newsitemlocation__location__id=place.id) |
-        #     Q(location_object__id=place.id))
-
-        # ... scratch all that, the trigger in 
-        # newsitemlocation_functions.sql appears to be what we want.
+        # This depends on the trigger in newsitemlocation.sql
         newsitem_qs = NewsItem.objects.filter(
             newsitemlocation__location__id=place.id)
 
