b7843ead97a4de7d8394ea53f3f54805a7d27344
Paul Winkler <slinkp@gmail.com>
2010-08-25 12:38:39 -0400

    little wrapper script for bootstrapping the demo from scratch

---
+++ obdemo/bin/bootstrap_demo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+HERE=`dirname $0`
+cd $HERE/../..
+
+echo Getting permission to run as postgres ...
+sudo -u postgres test
+
+echo Dropping openblock DB...
+dropdb openblock
+
+echo Bootstrapping...
+python bootstrap.py || exit 1
+source bin/activate || exit 1
+
+echo DB setup...
+sudo -u postgres bin/paver setup_db  || exit 1
+./manage.py syncdb || exit 1
+
+echo Importing Boston blocks...
+$HERE/import_boston_blocks.sh || exit 1
