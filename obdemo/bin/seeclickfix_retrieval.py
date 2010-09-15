15524532af524afea969a9afaffe2a98105d5777
Paul Winkler <slinkp@gmail.com>
2010-09-14 23:57:57 -0400

    seeclickfix scraper: better detection of existing records, cleaner description

--- obdemo/bin/seeclickfix_retrieval.py
+++ obdemo/bin/seeclickfix_retrieval.py
@@ -2,11 +2,14 @@ from django.contrib.gis.geos import Point
 from ebdata.retrieval.scrapers.list_detail import RssListDetailScraper
 from ebdata.retrieval.scrapers.newsitem_list_detail import NewsItemListDetailScraper
 from ebpub.db.models import NewsItem
+import re
 
 base_url = 'https://seeclickfix.com/api/'
 
 list_url = base_url + 'issues.rss?at=Boston,+MA'
 
+address_re = re.compile(r'Address: (.*?)<br\s+/>')
+rating_re = re.compile(r'\s+Rating:\s+\d\s*')
 
 class SeeClickFixNewsFeedScraper(RssListDetailScraper, NewsItemListDetailScraper):
     schema_slugs = ('local-news',) # TODO: make another type
@@ -27,18 +30,6 @@ class SeeClickFixNewsFeedScraper(RssListDetailScraper, NewsItemListDetailScraper
 
     def save(self, old_record, list_record, detail_record):
         kwargs = self.pk_fields(list_record)
-        summary_detail = list_record['summary_detail']['value']
-        content = list_record['summary']
-        # remove address and rating from content, i guess.
-        content = content.replace(summary_detail, '')
-        import re
-        address_re = re.compile(r'Address: (.*?)<br />')
-        addr = address_re.search(summary_detail)
-        if addr:
-            addr = addr.group(1)
-            location_name = ', '.join([part.strip() for part in addr.split(',')])
-        else:
-            location_name = u''
 
         location = Point((float(list_record['geo_long']),
                           float(list_record['geo_lat'])))
@@ -47,11 +38,16 @@ class SeeClickFixNewsFeedScraper(RssListDetailScraper, NewsItemListDetailScraper
             print "skipping %r as it has bad location 0,0" % list_record['title']
             return
 
-        kwargs.update(dict(
-                description=list_record['summary_detail']['value'],
-                location_name=location_name,
-                location=location,
-                ))
+        # remove address and rating from content, i guess.
+        summary_detail = list_record['summary_detail']['value']
+        content = list_record['summary']
+        content = address_re.sub('', content)
+        content = rating_re.sub('', content)
+        # TODO: strip remaining HTML from the description.
+        # TODO: save the rating in a field.
+        kwargs.update(dict(description=content,
+                           location=location,
+                           ))
         if old_record:
             self.update_existing(old_record, kwargs, {})
         else:
@@ -62,7 +58,16 @@ class SeeClickFixNewsFeedScraper(RssListDetailScraper, NewsItemListDetailScraper
         # purposes these are the fields that uniquely idenfity an article.
         import datetime
         date = datetime.date(*list_record['updated_parsed'][:3])
+        summary_detail = list_record['summary_detail']['value']
+        addr = address_re.search(summary_detail)
+        if addr:
+            addr = addr.group(1)
+            location_name = ', '.join([part.strip() for part in addr.split(',')])
+        else:
+            location_name = u''
+
         return dict(item_date=date,
+                    location_name=location_name,
                     title=u'SeeClickFix: ' + list_record['title'],
                     )
 
