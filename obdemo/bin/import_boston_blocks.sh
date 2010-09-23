74eef50315df68bdaae2b1eca5cd00ceb2ca2dbc
Luke Tucker <ltucker@openplans.org>
2010-09-23 12:22:42 -0400

    import better boston neighborhood shapefiles in the demo

--- obdemo/bin/import_boston_blocks.sh
+++ obdemo/bin/import_boston_blocks.sh
@@ -44,60 +44,7 @@ echo Importing blocks, this may take several minutes ...
 
 $IMPORTER tl_2009_25025_edges.shp tl_2009_25025_featnames.dbf tl_2009_25025_faces.dbf tl_2009_25_place.shp || die
 
-echo Creating a "'neighborhood'" location type...
-cd $SOURCE_ROOT/misc/bin || die
-
-./add_locationtype.py Neighborhood Neighborhoods neighborhoods neighborhoods || die
-
-echo Importing neighborhoods, this may take a minute ...
-
-cd $SOURCE_ROOT/misc/bin || die
-
-./add_location.py "Beacon Hill" "Beacon Hill" beacon-hill neighborhoods Boston http://bostonneighborhoodmap.com/ "42.355282, -71.073530" "42.361259, -71.059794"
-
-./add_location.py "West End" "West End" west-end neighborhoods Boston http://bostonneighborhoodmap.com/ "42.361543, -71.072498" "42.368279, -71.058504"
-
-./add_location.py "North End" "North End" north-end neighborhoods Boston http://bostonneighborhoodmap.com/ "42.360009, -71.061507" "42.368826, -71.049185"
-
-./add_location.py "Back Bay" "Back Bay" back-bay neighborhoods Boston http://bostonneighborhoodmap.com/ "42.345222, -71.091719" "42.35605, -71.070731"
-
-./add_location.py "Bay Village" "Bay Village" bay-village neighborhoods Boston http://bostonneighborhoodmap.com/ "42.346678, -71.072421" "42.351023, -71.066612"
-
-./add_location.py Charlestown Charlestown charleston neighborhoods Boston http://bostonneighborhoodmap.com/ "42.369953, -71.076024" "42.386476, -71.047983"
-
-./add_location.py "East Boston" "East Boston" east-boston neighborhoods Boston http://bostonneighborhoodmap.com/ "42.3472, -71.0443" "42.3952, -70.9853"
-
-./add_location.py "South Boston" "South Boston" south-boston neighborhoods Boston http://bostonneighborhoodmap.com/ "42.322102, -71.058578" "42.344339, -71.009413"
-
-./add_location.py Waterfront Waterfront waterfront neighborhoods Boston http://bostonneighborhoodmap.com/ "42.338576, -71.054973" "42.354695, -71.023110"
-
-./add_location.py "Fenway/Kenmore" "Fenway/Kenmore" fenway-kenmore neighborhoods Boston http://bostonneighborhoodmap.com/ "42.337880, -71.106093" "42.353080, -71.084205"
-
-./add_location.py Downtown Downtown downtown neighborhoods Boston http://bostonneighborhoodmap.com/ "42.350701, -71.064736" "42.362947, -71.051308"
-
-./add_location.py Chinatown Chinatown chinatown neighborhoods Boston http://bostonneighborhoodmap.com/ "42.347111, -71.066308" "42.352064, -71.053933"
-
-./add_location.py "South End" "South End" south-end neighborhoods Boston http://bostonneighborhoodmap.com/ "42.332086, -71.087094" "42.348450, -71.058869"
-
-./add_location.py "Mission Hill" "Mission Hill" mission-hill neighborhoods Boston http://bostonneighborhoodmap.com/ "42.326014, -71.112956" "42.342606, -71.092413"
-
-./add_location.py "Jamaica Plain" "Jamaica Plain" jamaica-plain neighborhoods Boston http://bostonneighborhoodmap.com/ "42.296080, -71.131307" "42.326367, -71.099157"
-
-./add_location.py Roxbury Roxbury roxbury neighborhoods Boston http://bostonneighborhoodmap.com/ "42.282421, -71.107001" "42.337916, -71.062619"
-
-./add_location.py Mattapan Mattapan mattapan neighborhoods Boston http://bostonneighborhoodmap.com/ "42.260214, -71.115589" "42.292218, -71.067432"
-
-./add_location.py Dorchester Dorchester dorchester neighborhoods Boston http://bostonneighborhoodmap.com/ "42.270124, -71.089461" "42.328234, -71.034102"
-
-./add_location.py Allston Allston allston neighborhoods Boston http://bostonneighborhoodmap.com/ "42.338822, -71.143509" "42.372874, -71.113447"
-
-./add_location.py Brighton Brighton brighton neighborhoods Boston http://bostonneighborhoodmap.com/ "42.335221, -71.172983" "42.364556, -71.144560"
-
-./add_location.py "Hyde Park" "Hyde Park" hyde-park neighborhoods Boston http://bostonneighborhoodmap.com/ "42.230844, -71.14604" "42.277670, -71.110062"
-
-./add_location.py "West Roxbury" "West Roxbury" west-roxbury neighborhoods Boston http://bostonneighborhoodmap.com/ "42.242338, -71.190201" "42.304530, -71.125352"
-
-./add_location.py Roslindale Roslindale roslindale neighborhoods Boston http://bostonneighborhoodmap.com/ "42.270984, -71.146519" "42.296778, -71.106192"
+#########################
 
 echo Populating streets and fixing addresses...
 
---
+++ obdemo/bin/import_boston_hoods.sh
@@ -0,0 +1,36 @@
+SOURCE_ROOT=`dirname $0`
+SOURCE_ROOT=`cd $SOURCE_ROOT/../.. && pwd`
+echo Source root is $SOURCE_ROOT
+
+export DJANGO_SETTINGS_MODULE=obdemo.settings
+
+
+function die {
+    echo $@ >&2
+    echo Exiting.
+    exit 1
+}
+
+HOOD_SERVER="http://developer.openblockproject.org/raw-attachment/ticket/62"
+HOOD_FILE="Planning_districts_revised.zip" 
+HOOD_URL="$HOOD_SERVER/$HOOD_FILE"
+HOOD_FOLDER="$PWD/neighborhood_data"
+HOOD_IMPORTER=$SOURCE_ROOT/ebpub/ebpub/db/bin/import_hoods.py
+
+if [ ! -f "$HOOD_IMPORTER" ]; then die "Could not find import_hoods.py at $HOOD_IMPORTER" ; fi
+
+
+echo Downloading neighborhood data...
+
+cd $PWD
+mkdir -p $HOOD_FOLDER
+cd $HOOD_FOLDER || die "couldn't cd to $HOOD_FOLDER"
+
+wget -N $HOOD_URL
+if [ $? -ne 0 ]; then
+    die "Could not download $HOOD_URL"
+fi
+unzip "$HOOD_FOLDER/$HOOD_FILE"
+
+echo Importing neighborhoods...
+$HOOD_IMPORTER -n PD $HOOD_FOLDER || die
