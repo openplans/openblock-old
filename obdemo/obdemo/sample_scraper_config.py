809e7e4a81456d12e2758ce3da23fbebbc5eb592
Paul Winkler <slinkp@gmail.com>
2010-09-30 17:11:07 -0400

    allow using already-set DJANGO_SETTINGS_MODULE env var

--- obdemo/obdemo/sample_scraper_config.py
+++ obdemo/obdemo/sample_scraper_config.py
@@ -1,10 +1,9 @@
 """
 Sample config file for the updaterdaemon.
 
-
 """
 
-
+import os
 from ebdata.retrieval.updaterdaemon.config import hourly
 
 def do_seeclickfix(**kwargs):
@@ -15,7 +14,7 @@ def do_aggregates(**kwargs):
     from ebpub.db.bin import update_aggregates
     return update_aggregates.update_all_aggregates(**kwargs)
 
-env = {'DJANGO_SETTINGS_MODULE': 'obdemo.settings'}
+env = {'DJANGO_SETTINGS_MODULE': os.environ.get('DJANGO_SETTINGS_MODULE', 'obdemo.settings')}
 
 TASKS = (
     # Tuples like (time_callback, function_to_run, {keyword args}, {environ})
@@ -23,7 +22,8 @@ TASKS = (
     # The time_callback should take a datetime instance and return True
     # if the function_to_run should be run, and False otherwise.
     #
-    # The environ should include DJANGO_SETTINGS_MODULE.
+    # The environ should include DJANGO_SETTINGS_MODULE, if os.environ
+    # doesn't already have it.
     #
     # Example:
     # (daily(12, 0), run_some_function, {'arg': 'foo'}, {'DJANGO_SETTINGS_MODULE': 'foo.settings'})
