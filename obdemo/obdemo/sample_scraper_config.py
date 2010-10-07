a7808199baaeb42df7aaf720fc5b5b2142b47378
Paul Winkler <slinkp@gmail.com>
2010-10-06 22:44:35 -0400

    Added: Run all our scrapers.

--- obdemo/obdemo/sample_scraper_config.py
+++ obdemo/obdemo/sample_scraper_config.py
@@ -10,9 +10,24 @@ def do_seeclickfix(**kwargs):
     from obdemo.scrapers.seeclickfix_retrieval import SeeClickFixNewsFeedScraper
     return SeeClickFixNewsFeedScraper().update()
 
+def do_events(**kwargs):
+    from obdemo.scrapers.add_events import main
+    return main()
+
+def do_news(**kwargs):
+    from obdemo.scrapers.add_news import main
+    main()
+    main(["http://search.boston.com/search/api?q=*&sort=-articleprintpublicationdate&scope=blogs&count=400&subject=massachusetts&format=atom"])
+
+
+def do_press_releases(**kwargs):
+    from obdemo.scrapers.bpdnews_retrieval import BPDNewsFeedScraper
+    return BPDNewsFeedScraper().update()
+
 def do_aggregates(**kwargs):
     from ebpub.db.bin import update_aggregates
     return update_aggregates.update_all_aggregates(**kwargs)
+
 
 env = {'DJANGO_SETTINGS_MODULE': os.environ.get('DJANGO_SETTINGS_MODULE', 'obdemo.settings')}
 
@@ -27,7 +42,11 @@ TASKS = (
     #
     # Example:
     # (daily(12, 0), run_some_function, {'arg': 'foo'}, {'DJANGO_SETTINGS_MODULE': 'foo.settings'})
-    (hourly(*range(0, 60, 2)), do_seeclickfix, {}, env),
-    (hourly(*range(1, 60, 2)), do_aggregates, {'verbose': True}, env),
+    (hourly(*range(0, 60, 15)), do_news, {}, env),
+    (hourly(*range(1, 60, 15)), do_seeclickfix, {}, env),
+    (hourly(*range(2, 60, 15)), do_events, {}, env),
+    (hourly(*range(3, 60, 15)), do_press_releases, {}, env),
+
+    (hourly(*range(4, 60, 15)), do_aggregates, {'verbose': False}, env),
 )
 
