50f6fbb6af485e8d6199dad778644a06211c87be
Paul Winkler <slinkp@gmail.com>
2010-10-27 12:56:51 -0400

    Fix assumption that source root == virtualenv root

--- obdemo/bin/add_boston_news_schemas.sh
+++ obdemo/bin/add_boston_news_schemas.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-SOURCE_ROOT=`dirname $0`
-SOURCE_ROOT=`cd $SOURCE_ROOT/../.. && pwd`
-echo Source root is $SOURCE_ROOT
-
 export DJANGO_SETTINGS_MODULE=obdemo.settings
 
+if [ ! -n "$VIRTUAL_ENV" ]; then
+    echo Need to activate your virtualenv first.
+    exit 1
+fi
+
+HERE=`(cd "${0%/*}" 2>/dev/null; echo "$PWD"/)`
+SOURCE_ROOT=`cd $HERE/../.. && pwd`
+echo Source root is $SOURCE_ROOT
+
+
 echo Set up news type schemas ...
-cd $SOURCE_ROOT
-echo in $PWD
+cd $VIRTUAL_ENV
 ./manage.py loaddata $SOURCE_ROOT/obdemo/obdemo/fixtures/boston_schemas.json
 
