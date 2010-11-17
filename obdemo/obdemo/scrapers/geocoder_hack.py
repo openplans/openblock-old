6bc08c31ecba726b0b6d4a9d916c4f0b9e50753b
Paul Winkler <slinkp@gmail.com>
2010-11-17 14:30:08 -0500

    remove unused geocoder cruft

--- obdemo/obdemo/scrapers/geocoder_hack.py
+++ obdemo/obdemo/scrapers/geocoder_hack.py
@@ -33,27 +33,6 @@ def quick_dirty_fallback_geocode(addr, parse=True):
                 # now till we figure out what to do with geocoding
                 # more generally
                 continue
-            if None in (x, y):
-                logger.debug("Internal geocoder failed; trying others")
-                # Other geocoders need to know the city
-                addr += ', Boston, MA'
-                from geopy import geocoders
-                g = geocoders.Google(resource='maps', output_format='json')
-                import urllib2
-                try:
-                    for unused, (lat, lon) in g.geocode(addr, exactly_one=False):
-                        logger.debug("google geocoded %r" % addr)
-                        return (lon, lat)
-                except urllib2.HTTPError:
-                    # Rate throttled? Try another.
-                    pass
-                except ValueError:
-                    # Bad JSON response? why?
-                    pass
-                us = geocoders.GeocoderDotUS()
-                for unused, (lat, lon) in us.geocode(addr, exactly_one=False):
-                    logger.debug("geocoder.us geocoded %r" % addr)
-                    return (lon, lat)
         except:
             logger.error('uncaught geocoder exception on %r\n' % addr)
             log_exception()
