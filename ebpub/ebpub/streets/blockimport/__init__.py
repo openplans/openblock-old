1d7c2b6ed77706dbc26a80a539866250ab720bce
Paul Winkler <slinkp@gmail.com>
2010-12-10 15:09:32 -0500

    TIGER block importer fixes ported from recent ESRI fixes:  - decode strings w/ a configurable encoding  - factor out logging  - sanitize address numbers a bit (handles eg. '27B' and '27-29')

--- ebpub/ebpub/streets/blockimport/__init__.py
+++ ebpub/ebpub/streets/blockimport/__init__.py
@@ -1,1 +1,1 @@
-from base import BlockImporter # relative import
+#
--- ebpub/ebpub/streets/blockimport/base.py
+++ ebpub/ebpub/streets/blockimport/base.py
@@ -1,3 +1,5 @@
+import string
+import sys
 from django.contrib.gis.gdal import DataSource
 from ebpub.streets.models import Block
 from ebpub.streets.name_utils import make_pretty_name
@@ -5,30 +7,37 @@ from ebpub.streets.name_utils import make_block_numbers
 from ebpub.utils.text import slugify
 
 class BlockImporter(object):
-    def __init__(self, shapefile, layer_id=0):
+    def __init__(self, shapefile, layer_id=0, verbose=False, encoding='utf8'):
         self.layer = DataSource(shapefile)[layer_id]
+        self.verbose = verbose
+        self.encoding = encoding
 
-    def save(self, verbose=True):
+    def log(self, arg):
+        if self.verbose:
+            print >> sys.stderr, arg
+
+    def save(self):
         num_created = 0
         for feature in self.layer:
             parent_id = None
             if not self.skip_feature(feature):
                 for block_fields in self.gen_blocks(feature):
+
                     # Usually (at least in Boston data) there is only
                     # 1 block per feature.  But sometimes there are
                     # multiple names for one street, eg.
                     # "N. Commercial Wharf" and "Commercial Wharf N.";
                     # in that case those would be yielded by gen_blocks() as
                     # two separate blocks. Is that intentional, or a bug?
-                    block = Block(**block_fields)
-                    block.geom = feature.geom.geos
-                    (block.from_num, block.to_num) = make_block_numbers(
-                        block_fields['left_from_num'],
-                        block_fields['left_to_num'],
-                        block_fields['right_from_num'],
-                        block_fields['right_to_num'])
 
-                    street_name, block_name = make_pretty_name(
+                    # Ensure we have unicode.
+                    for key, val in block_fields.items():
+                        if isinstance(val, str):
+                            block_fields[key] = val.decode(self.encoding)
+
+                    block_fields['geom'] = feature.geom.geos
+
+                    block_fields['street_pretty_name'], block_fields['pretty_name'] = make_pretty_name(
                         block_fields['left_from_num'],
                         block_fields['left_to_num'],
                         block_fields['right_from_num'],
@@ -38,9 +47,38 @@ class BlockImporter(object):
                         block_fields['suffix'],
                         block_fields['postdir']
                     )
-                    block.pretty_name = block_name
-                    block.street_pretty_name = street_name
-                    block.street_slug = slugify(' '.join((block_fields['street'], block_fields['suffix'])))
+
+                    block_fields['street_slug'] = slugify(u' '.join((block_fields['street'], block_fields['suffix'])))
+
+                    # Watch out for addresses like '247B' which can't be
+                    # saved as an IntegerField.
+                    # But do this *after* making pretty names.
+                    for addr_key in ('left_from_num', 'left_to_num',
+                                     'right_from_num', 'right_to_num'):
+                        if isinstance(block_fields[addr_key], basestring):
+                            value = block_fields[addr_key].rstrip(string.letters)
+                            # Also attempt to fix up addresses like
+                            # '19-47', by just using the lower number.
+                            # This will give misleading output, but
+                            # it's probably better than discarding blocks.
+                            value = value.split('-')[0]
+                            if value:
+                                try:
+                                    value = int(value)
+                                except ValueError:
+                                    self.log("Omitting weird value %r for %r" % (value, addr_key))
+                                    value = None
+                            else:
+                                value = None
+                            block_fields[addr_key] = value
+
+                    block_fields['from_num'], block_fields['to_num'] = make_block_numbers(
+                        block_fields['left_from_num'],
+                        block_fields['left_to_num'],
+                        block_fields['right_from_num'],
+                        block_fields['right_to_num'])
+
+                    block = Block(**block_fields)
                     block.save()
                     if parent_id is None:
                         parent_id = block.id
@@ -48,8 +86,7 @@ class BlockImporter(object):
                         block.parent_id = parent_id
                         block.save()
                     num_created += 1
-                    if verbose:
-                        print '%d\tCreated block %s for feature %d' % (num_created, block, feature.get('TLID'))
+                    self.log('%d\tCreated block %s for feature %d' % (num_created, block, feature.get('TLID')))
         return num_created
 
     def skip_feature(self, feature):
