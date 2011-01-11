35e5f7e5a746a2bcc34bebda2d56119e2b6b91d8
Paul Winkler <slinkp@gmail.com>
2011-01-11 11:52:51 -0500

    comment re. possibly unused model

--- ebpub/ebpub/streets/models.py
+++ ebpub/ebpub/streets/models.py
@@ -314,6 +314,7 @@ class StreetMisspelling(models.Model):
 
 # A generic place, like "Millennium Park" or "Sears Tower"
 class Place(models.Model):
+    # XXX Is this actually used for anything? doesn't look like it?
     pretty_name = models.CharField(max_length=255)
     normalized_name = models.CharField(max_length=255) # Always uppercase, single spaces
     address = models.CharField(max_length=255, blank=True)
