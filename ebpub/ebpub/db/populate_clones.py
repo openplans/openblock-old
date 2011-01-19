26a00242a9be6da35de9f58d74f957c792a638a7
Paul Winkler <slinkp@gmail.com>
2011-01-18 22:37:29 -0500

    attributes conversion script fixes

--- ebpub/ebpub/db/populate_clones.py
+++ ebpub/ebpub/db/populate_clones.py
@@ -1,7 +1,7 @@
 from django.db import transaction
+from ebpub.db import models
 
 def populate_new_from_old_all(delete=False):
-    from ebpub.db import models
     for model in (models.TestyIssuesModel,):
         print "Deleting all existing of new model %s" % (model.schemaslug,)
         model.objects.all().delete()
@@ -13,8 +13,7 @@ def populate_one_new_from_old(model, delete=False):
     Populate the given model by cloning instances of old-school NewsItem that
     have the schema designated by model.schemaslug
     """
-    import ebpub.db.models
-    items = list(ebpub.db.models.NewsItem.objects.filter(schema__slug=model.schemaslug))
+    items = list(models.NewsItem.objects.filter(schema__slug=model.schemaslug))
     for i, item in enumerate(items):
         new_item = model(schema=item.schema, title='NEW MODEL ' + item.title,
                          description=item.description, url=item.url,
@@ -58,27 +57,48 @@ def populate_one_new_from_old(model, delete=False):
         # delete from db_newsitem where schema_id = 5 and id not in (select newsitem_ptr_id from db_testyissuesmodel);
 
 
-def populate_old_model_from_new(model, delete=False):
+def populate_old_model_from_new(model, clone=False, delete=False):
     """
-    Populate old-school NewsItems with Attributes by cloning instances
-    of the new-school `model`.
+    Populate old-school NewsItems with Attributes by either just making
+    and linking to an Attributes instance if clone==False,
+    or cloning full instances of the new-school `model` if clone==True.
+
+    If delete==True, instances of new-school model will be deleted too.
     """
-    import ebpub.db.models
     items = list(model.objects.all())
-    parent_model = ebpub.db.models.NewsItem
+    parent_model = models.NewsItem
     for i, item in enumerate(items):
-        new_item = parent_model(
-                         schema=item.schema, title='NEW MODEL ' + item.title,
-                         description=item.description, url=item.url,
-                         pub_date=item.pub_date, item_date=item.item_date,
-                         location=item.location,
-                         location_name=item.location_name,
-                         location_object=item.location_object,
-                         block=item.block)
-        new_item.save()
+        if clone:
+
+            new_item = parent_model(
+                             schema=item.schema, title='NEW MODEL ' + item.title,
+                             description=item.description, url=item.url,
+                             pub_date=item.pub_date, item_date=item.item_date,
+                             location=item.location,
+                             location_name=item.location_name,
+                             location_object=item.location_object,
+                             block=item.block)
+            new_item.save()
+            print "Creating old-style clone %s" % new_item
+        else:
+            new_item = item.newsitem_ptr
+            print "Saving old-style attributes for %s" % new_item
         if len(item.attributes):
-            # XXX TODO: fix Lookups
-            new_item.attributes = dict(item.attributes)
-    print "Saved %d NewsItems" % i + 1
+            attrs = dict(item.attributes)
+            for sf in item.schema.schemafield_set.all():
+                val = attrs[sf.name]
+                key = sf.name
+                if sf.is_many_to_many_lookup():
+                    val = ','.join([str(s.id) for s in val.all()])
+                elif sf.is_lookup:
+                    val = val.all()
+                    if val:
+                        val = val[0].id
+                    else:
+                        val = None
+                attrs[key] = val
+            new_item.attributes = attrs
+
+    print "Saved %d NewsItems" % (i + 1)
     if delete:
         model.objects.all().delete()
