fc0e204874974a465da9f1dbcf4367afa29d7043
Paul Winkler <slinkp@gmail.com>
2011-01-25 11:55:05 -0500

    Adding a sql script to conveniently clear out all newsitems, schemas, and related tables.

---
+++ ebpub/ebpub/db/sql/reset_schemas_and_newsitems.sql
@@ -0,0 +1,14 @@
+
+truncate table db_attribute cascade;
+truncate table db_lookup cascade;
+truncate table db_newsitem cascade;
+truncate table db_newsitemlocation cascade;
+truncate table db_schemafield cascade;
+truncate table db_schema cascade;
+truncate table db_aggregate cascade;
+
+alter sequence db_lookup_id_seq restart with 1;
+alter sequence db_attribute_id_seq restart with 1;
+alter sequence db_schemafield_id_seq restart with 1;
+alter sequence db_schema_id_seq restart with 1;
+alter sequence db_newsitem_id_seq restart with 1;
