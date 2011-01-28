4680a16c9dec38f019cdf1c933991b4699f00cee
Paul Winkler <slinkp@gmail.com>
2011-01-28 11:54:46 -0500

    Workaround for "GEOS intersects() threw an error"

--- ebpub/ebpub/db/sql/location_post_syncdb.sql
+++ ebpub/ebpub/db/sql/location_post_syncdb.sql
@@ -5,17 +5,21 @@ ALTER TABLE db_location ALTER COLUMN location SET STATISTICS 75;
 -- The only restriction is that possible collection geometry is the second argument.
 -- Ending statements with "--" as workaround described at http://code.djangoproject.com/ticket/3214.
 
+-- Using ST_Buffer(geom, 0) as a workaround for some geometries
+-- that blow up ST_Intersects with "GEOS intersects() threw an error!",
+-- as per http://www.mail-archive.com/postgis-users@postgis.refractions.net/msg10143.html
+
 CREATE OR REPLACE FUNCTION intersecting_collection(other geometry, possible_coll geometry) RETURNS boolean AS $$
     BEGIN
         IF (GeometryType(possible_coll) = 'GEOMETRYCOLLECTION') THEN
             FOR i IN 1..ST_NumGeometries(possible_coll) LOOP
-                IF ST_Intersects(ST_GeometryN(possible_coll, i), other) = 't' THEN
+                IF ST_Intersects(ST_GeometryN(possible_coll, i), ST_Buffer(other, 0)) = 't' THEN
                     RETURN 't'; --
                 END IF; --
             END LOOP; --
             RETURN 'f'; --
         ELSE
-            RETURN ST_Intersects(possible_coll, other); --
+            RETURN ST_Intersects(ST_Buffer(possible_coll, 0), ST_Buffer(other, 0)); --
         END IF; --
     END; --
 $$ LANGUAGE plpgsql; --
