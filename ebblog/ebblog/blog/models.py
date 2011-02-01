99a5c0cfe3d692acac5293413a681ef273a84a27
Paul Winkler <slinkp@gmail.com>
2011-01-31 20:01:17 -0500

    Adding copyrights and GPL v3 everywhere

--- ebblog/ebblog/blog/models.py
+++ ebblog/ebblog/blog/models.py
@@ -1,3 +1,21 @@
+#   Copyright 2007,2008,2009 Everyblock LLC
+#
+#   This file is part of ebblog
+#
+#   ebblog is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   ebblog is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with ebblog.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from django.db import models
 
 class Entry(models.Model):
--- ebwiki/ebwiki/wiki/feeds.py
+++ ebwiki/ebwiki/wiki/feeds.py
@@ -1,3 +1,21 @@
+#   Copyright 2007,2008,2009 Everyblock LLC
+#
+#   This file is part of ebwiki
+#
+#   ebwiki is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   ebwiki is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with ebwiki.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from django.contrib.syndication.feeds import Feed
 from django.utils.feedgenerator import Atom1Feed
 from ebwiki.wiki.models import Page
