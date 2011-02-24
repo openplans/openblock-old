3b1e821a311e94ee871d5331a5f415454e6494e9
Luke Tucker <voxluci@gmail.com>
2011-02-24 13:16:06 -0500

    jsonp support, test

--- ebpub/ebpub/openblockapi/views.py
+++ ebpub/ebpub/openblockapi/views.py
@@ -29,13 +29,15 @@ def APIGETResponse(request, body, **kw):
     if jsonp is None: 
         return HttpResponse(body, **kw)
     else: 
-        content_type = kw.get("mimetype", kw.get("content_type"), "text/plain")
+        content_type = kw.get("mimetype", kw.get("content_type", "text/plain"))
         if content_type == JSON_CONTENT_TYPE:
             body = jsonp + "(" + body + ");" 
         else: 
-            body = jsonp + "(" + simplejson.dumps(body) + ");"
-        del kw['content_type']
-        del kw['mimetype']
+            body = jsonp + "(" + simplejson.dumps(body, indent=1) + ");"
+        if 'content_type' in kw:
+            del kw['content_type']
+        if 'mimetype' in kw:
+            del kw['mimetype']
         kw['content_type'] = JAVASCRIPT_CONTENT_TYPE
         return HttpResponse(body, **kw)
 
@@ -109,7 +111,7 @@ def _items_json(items):
         }
         result['features'].append(item)
     
-    return simplejson.dumps(result)
+    return simplejson.dumps(result, indent=1)
 
 def _items_atom(items):
     # XXX
@@ -124,7 +126,7 @@ def geocode(request):
                 
     collection = {'type': 'FeatureCollection',
                   'features': _geocode_geojson(q)}
-    return APIGETResponse(request, simplejson.dumps(collection), mimetype="application/json")
+    return APIGETResponse(request, simplejson.dumps(collection, indent=1), mimetype="application/json")
 
 def _geocode_geojson(query):
     if not query: 
@@ -221,9 +223,8 @@ def list_types_json(request):
                 'attributes': attributes,
                 }
 
-    response = HttpResponse(mimetype="application/javascript")
-    simplejson.dump(schemas, response, indent=1)
-    return response
+    return APIGETResponse(request, simplejson.dumps(schemas, indent=1),
+                          content_type='application/json')
 
 def locations_json(request):
     # TODO: this will obsolete ebpub.db.views.ajax_location_list
@@ -235,9 +236,9 @@ def locations_json(request):
          'url': reverse('location_detail_json', kwargs={'slug': loc.slug, 'loctype': loc.location_type.slug})
          }
         for loc in locations]
-    response = HttpResponse(mimetype='application/javascript')
-    simplejson.dump(loc_objs, response, indent=1)
-    return response
+
+    return APIGETResponse(request, simplejson.dumps(loc_objs, indent=1),
+                          content_type='application/json')
 
 def location_detail_json(request, loctype, slug):
     # TODO: this will obsolete ebpub.db.views.ajax_location
@@ -264,8 +265,7 @@ def location_detail_json(request, loctype, slug):
                               }
                }
     geojson = simplejson.dumps(geojson, indent=1)
-    response = HttpResponse(geojson, mimetype='application/javascript')
-    return response
+    return APIGETResponse(request, geojson, content_type='application/json')
 
 def location_types_json(request):
     typelist = models.LocationType.objects.order_by('name').values(
@@ -273,6 +273,6 @@ def location_types_json(request):
     typedict = {}
     for typeinfo in typelist:
         typedict[typeinfo.pop('slug')] = typeinfo
-    response = HttpResponse(mimetype='application/javascript')
-    simplejson.dump(typedict, response, indent=1)
-    return response
+
+    return APIGETResponse(request, simplejson.dumps(typedict, indent=1),
+                         content_type='application_json')
