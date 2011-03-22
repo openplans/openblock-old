2ca301be8b7257f1a44b5d9aae7ed96ad17b8537
Paul Winkler <slinkp@gmail.com>
2011-03-22 17:06:00 -0400

    Oops, we are going to need south enabled during tests if it's the thing that manages custom sql now. Of course, this breaks tests currently

--- ebpub/ebpub/settings_default.py
+++ ebpub/ebpub/settings_default.py
@@ -120,8 +120,8 @@ INSTALLED_APPS = INSTALLED_APPS + APPS_FOR_TESTING + ('south',)
 TEST_RUNNER = 'obadmin.testrunner.TestSuiteRunner'
 
 # South - database migration config
-SKIP_SOUTH_TESTS=True
-SOUTH_TESTS_MIGRATE = False
+SKIP_SOUTH_TESTS = True
+SOUTH_TESTS_MIGRATE = True
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.gzip.GZipMiddleware',
