b8c9276195ce9e77e3eea81d40090a37df753997
Paul Winkler <slinkp@gmail.com>
2011-03-22 12:13:51 -0400

    at least comment what this custom sql does

--- ebpub/ebpub/db/sql/attribute.sql
+++ ebpub/ebpub/db/sql/attribute.sql
@@ -1,1 +1,11 @@
+-- AFAICT this is a query-planner optimization: The default STATISTICS
+-- level is 10.  Lowering it means: Do less work generating stats on
+-- this column, because it has a "simple distribution" (just a few
+-- possible integer values), so we don't need to expend much time or
+-- space on helping out the query planner.
+-- Presumably, somebody at everyblock.com found that this improved
+-- performance.
+-- See http://www.postgresql.org/docs/8.2/static/planner-stats.html
+-- and http://www.postgresql.org/docs/8.2/static/runtime-config-query.html
+
 ALTER TABLE db_attribute ALTER COLUMN schema_id SET STATISTICS 5;
