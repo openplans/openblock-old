openplans/openblock-old

commit a11a8b00a22fa6cff800add32dd02168b6c1ee4d

Lots more columns, filters, and usable default ordering in admin UI; closes #100, closes #103, closes #104

ebpub/ebpub/db/models.py
@@ -138,6 +138,8 @@ class Schema(models.Model):
                                         choices=FREQUENCY_CHOICES)
     intro = models.TextField(blank=True, default='')
 
+    class Meta:
+        ordering = ('name',)
 
 class SchemaFieldManager(models.Manager):
 
@@ -167,6 +169,7 @@ class SchemaField(models.Model):
 
     class Meta(object):
         unique_together = (('schema', 'real_name'),)
+        ordering = ('pretty_name',)
 
     def __unicode__(self):
         return u'%s - %s' % (self.schema, self.name)
@@ -235,6 +238,9 @@ class LocationType(models.Model):
     def natural_key(self):
         return (self.slug,)
 
+    class Meta:
+        ordering = ('name',)
+
     objects = LocationTypeManager()
 
 
@@ -263,6 +269,7 @@ class Location(models.Model):
 
     class Meta:
         unique_together = (('slug', 'location_type'),)
+        ordering = ('slug',)
 
     def natural_key(self):
         return (self.slug, self.location_type.slug)
@@ -554,6 +561,9 @@ class NewsItem(models.Model):
     objects = NewsItemManager()
     attributes = AttributesDescriptor()  # Treat it like a dict.
 
+    class Meta:
+        ordering = ('title',)
+
     def __unicode__(self):
         return self.title
 
@@ -768,6 +778,7 @@ class Lookup(models.Model):
 
     class Meta:
         unique_together = (('slug', 'schema_field'),)
+        ordering = ('slug',)
 
     def natural_key(self):
         return (self.slug, self.schema_field.schema.slug,

ebpub/ebpub/streets/models.py
@@ -165,6 +165,7 @@ class Block(models.Model):
 
     class Meta:
         db_table = 'blocks'
+        ordering = ('pretty_name',)
 
     def __unicode__(self):
         return self.pretty_name
@@ -288,6 +289,7 @@ class Street(models.Model):
 
     class Meta:
         db_table = 'streets'
+        ordering = ('pretty_name',)
 
     def __unicode__(self):
         return self.pretty_name
@@ -374,6 +376,7 @@ class BlockIntersection(models.Model):
 
     class Meta:
         unique_together = ("block", "intersecting_block")
+        ordering = ('block',)
 
     def __unicode__(self):
         return u'%s intersecting %s' % (self.block, self.intersecting_block)
@@ -435,6 +438,7 @@ class Intersection(models.Model):
     class Meta:
         db_table = 'intersections'
         unique_together = ("predir_a", "street_a", "suffix_a", "postdir_a", "predir_b", "street_b", "suffix_b", "postdir_b")
+        ordering = ('slug',)
 
     def __unicode__(self):
         return self.pretty_name

ebpub/ebpub/db/admin.py
@@ -21,6 +21,7 @@ from ebpub.db.forms import LocationForm
 from ebpub.db.forms import NewsItemForm
 from ebpub.db.models import Attribute, Location, LocationType, NewsItem
 from ebpub.db.models import Schema, SchemaField
+from ebpub.db.models import Lookup
 
 from ebpub.geoadmin import OSMModelAdmin
 
@@ -37,6 +38,7 @@ class NewsItemAdmin(OSMModelAdmin):
     list_display = ('title', 'schema', 'item_date', 'pub_date', 'location_name')
     raw_id_fields = ('location_object', 'block')
     list_filter = ('schema',)
+    search_fields = ('title', 'description',)
     form = NewsItemForm
 
 
@@ -48,19 +50,33 @@ class LocationAdmin(OSMModelAdmin):
     form = LocationForm
 
     list_filter = ('location_type', 'city', 'is_public',)
-    list_display = ('name', 'location_type', 'creation_date',)
+    list_display = ('name', 'location_type', 'creation_date', 'area')
+    search_fields = ('name',)
 
     # This is populated by a trigger in ebpub/db/sql/location.sql.
     readonly_fields = ('area',)
 
+
+class SchemaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_updated', 'importance', 'is_public',
+                    'has_newsitem_detail',)
+
 class SchemaFieldAdmin(admin.ModelAdmin):
-    list_filter = ('schema', 'is_lookup', 'is_filter', 'is_charted', 'is_searchable')
+    list_display = ('pretty_name', 'real_name', 'display', 'is_lookup',
+                    'is_filter', 'is_charted', 'is_searchable')
+    list_filter = ('schema', 'display', 'is_lookup', 'is_filter', 'is_charted', 'is_searchable', 'real_name')
 
 
-admin.site.register(Schema)
+class LookupAdmin(admin.ModelAdmin):
+    # TODO: this would make more sense to edit inline on NewsItem,
+    # but that would require some custom wackiness.
+    list_display = ('name', 'schema_field')
+    search_fields = ('description', 'name', 'code')
+
+admin.site.register(Schema, SchemaAdmin)
 admin.site.register(SchemaField, SchemaFieldAdmin)
 #admin.site.register(Attribute, AttributeAdmin)
 admin.site.register(NewsItem, NewsItemAdmin)
 admin.site.register(LocationType, LocationTypeAdmin)
 admin.site.register(Location, LocationAdmin)
-
+admin.site.register(Lookup, LookupAdmin)

ebpub/ebpub/streets/admin.py
@@ -23,22 +23,36 @@ from ebpub.streets.models import Block, Street, BlockIntersection, \
 from ebpub.geoadmin import OSMModelAdmin
 
 class PlaceAdmin(OSMModelAdmin):
-    pass
+    list_display = ('pretty_name', 'address',)
+    search_fields = ('pretty_name',)
 
 class BlockAdmin(OSMModelAdmin):
-    pass
+    list_display = ('pretty_name', 'street', 'suffix', 'left_zip', 'right_zip', 'left_city', 'right_city')
+    list_filter = ('suffix', 'left_city', 'right_city', 'left_zip', 'right_zip')
+    search_fields = ('pretty_name',)
 
 class StreetAdmin(OSMModelAdmin):
-    pass
+    list_display = ('pretty_name', 'suffix', 'city', 'state',)
+    list_filter = ('suffix', 'city', 'state',)
+    search_fields = ('pretty_name',)
 
 class BlockIntersectionAdmin(OSMModelAdmin):
-    pass
+    list_display = ('block', 'intersecting_block', 'intersection',)
+    raw_id_fields = ('block', 'intersecting_block', 'intersection',)
+    search_fields = ('block__pretty_name',)
 
 class IntersectionAdmin(OSMModelAdmin):
-    pass
+    list_display = ('pretty_name', 'zip', 'city', 'state')
+    list_filter = ('zip', 'city', 'state')
+    search_fields = ('pretty_name',)
 
 class SuburbAdmin(OSMModelAdmin):
     pass
+
+class StreetMisspellingAdmin(OSMModelAdmin):
+    list_display = ('incorrect', 'correct',)
+    search_fields = ('incorrect', 'correct',)
+
 
 admin.site.register(Block, BlockAdmin)
 admin.site.register(Street, StreetAdmin)
@@ -46,4 +60,4 @@ admin.site.register(BlockIntersection, BlockIntersectionAdmin)
 admin.site.register(Intersection, IntersectionAdmin)
 admin.site.register(Suburb, SuburbAdmin)
 admin.site.register(Place, PlaceAdmin)
-admin.site.register(StreetMisspelling)
+admin.site.register(StreetMisspelling, StreetMisspellingAdmin)

commit 9a402a11a3cf823d5a2a18ac6e6c673dcf5f417e

Merged: Fix #146: breakage was introduced in changeset a11a8b00 when we added a default ORDER BY to NewsItemQuerySet. This had side effect of adding a spurious GROUP BY in the top_lookups query, which caused multiple rows in a subquery, which is an error.

ebpub/ebpub/db/models.py
@@ -424,6 +424,7 @@ class AttributeDict(dict):
         dict.__setitem__(self, name, value)
 
 class NewsItemQuerySet(models.query.GeoQuerySet):
+
     def prepare_attribute_qs(self):
         clone = self._clone()
         if 'db_attribute' not in clone.query.extra_tables:
@@ -497,7 +498,14 @@ class NewsItemQuerySet(models.query.GeoQuerySet):
         """
         real_name = "db_attribute." + str(schema_field.real_name)
         if schema_field.is_many_to_many_lookup():
-            clone = self.prepare_attribute_qs().filter(schema__id=schema_field.schema_id)
+            # First prepare a subquery to get a *single* count of
+            # attribute rows that match each relevant m2m lookup
+            # value.  It's very important to get a single row here or
+            # else we get a DataBaseError with "more than one row
+            # returned by a subquery used as an expression". (Bug #146)
+            clone = self.prepare_attribute_qs()
+            clone = clone.filter(schema__id=schema_field.schema_id)
+            # This is a regex search for the lookup id.
             clone = clone.extra(where=[real_name + " ~ ('[[:<:]]' || db_lookup.id || '[[:>:]]')"])
             # We want to count the current queryset and get a single
             # row for injecting into the subsequent Lookup query, but
@@ -507,16 +515,32 @@ class NewsItemQuerySet(models.query.GeoQuerySet):
             # `values()' on a field that we're already filtering by,
             # in this case, schema, as essentially a harmless identify
             # function.
-            clone = clone.values('schema').annotate(count=Count('schema'))
+            # See http://docs.djangoproject.com/en/dev/topics/db/aggregation/#values
+            clone = clone.values('schema')
+
+            # Fix #146: Having any `ORDER BY foo` in this subquery causes
+            # Django to also add a `GROUP BY foo`, which potentially
+            # returns multiple rows. So, remove the ordering.
+            clone = clone.order_by()
+            clone = clone.annotate(count=Count('schema'))
+            # Unusual: We don't run the clone query, we just stuff its SQL
+            # into our Lookup qs.
             qs = Lookup.objects.filter(schema_field__id=schema_field.id)
             qs = qs.extra(select={'lookup_id': 'id', 'item_count': clone.values('count').query})
         else:
+            # Counts of attribute rows matching each relevant Lookup.
+            # Much easier when is_many_to_many_lookup == False :-)
             qs = self.prepare_attribute_qs().extra(select={'lookup_id': real_name})
             qs.query.group_by = [real_name]
             qs = qs.values('lookup_id').annotate(item_count=Count('id'))
-        ids_and_counts = [(v['lookup_id'], v['item_count']) for v in qs.values('lookup_id', 'item_count').order_by('-item_count') if v['item_count']][:count]
+
+        qs = qs.values('lookup_id', 'item_count').order_by('-item_count')
+        ids_and_counts = [(v['lookup_id'], v['item_count']) for v in qs
+                          if v['item_count']]
+        ids_and_counts = ids_and_counts[:count]
         lookup_objs = Lookup.objects.in_bulk([i[0] for i in ids_and_counts])
-        return [{'lookup': lookup_objs[i[0]], 'count': i[1]} for i in ids_and_counts]
+        return [{'lookup': lookup_objs[i[0]], 'count': i[1]} for i in ids_and_counts
+                if not None in i]
 
     def text_search(self, schema_field, query):
         """