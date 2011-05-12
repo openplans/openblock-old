69f7fb8923ba19591b60d91cc98c12f95289145c
Paul Winkler <slinkp@gmail.com>
2011-05-11 22:39:29 -0400

    Make obdemo into a proper Django app (give it an empty models.py)

---
+++ obdemo/obdemo/models.py
@@ -0,0 +1,20 @@
+#   Copyright 2007,2008,2009,2011 Everyblock LLC, OpenPlans, and contributors
+#
+#   This file is part of obdemo
+#
+#   obdemo is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   obdemo is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with obdemo.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# There are no models, but we want to be able to write data migrations
+# for the demo, and South needs it to be a django app (i.e. have models.)
