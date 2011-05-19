b0bef37ce348896ef865ad54ec15a4d66409294d
Paul Winkler <slinkp@gmail.com>
2011-05-19 15:57:50 -0400

    Remove everyblock gunk from obdemo.urls

--- obdemo/obdemo/urls.py
+++ obdemo/obdemo/urls.py
@@ -22,14 +22,9 @@ from obadmin import admin
 
 admin.autodiscover()
 
-
 urlpatterns = patterns(
 
     '',
-
-    # EB admin site. Also need 2 additonal apps in settings.INSTALLED_APPS:
-    # 'everyblock.admin' and 'everyblock.staticmedia'.
-    #(r'^ebadmin/', include('everyblock.admin.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
