13bf565b2f98d802e14dc079f999d25824a71bae
Paul Winkler <slinkp@gmail.com>
2011-06-20 16:29:47 -0400

    Add backports of django 1.4 (dev) settings override features for tests

---
+++ ebpub/ebpub/utils/django_testcase_backports.py
@@ -0,0 +1,101 @@
+# Copyright (c) Django Software Foundation and individual contributors.
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+
+#     1. Redistributions of source code must retain the above copyright notice, 
+#        this list of conditions and the following disclaimer.
+
+#     2. Redistributions in binary form must reproduce the above copyright 
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+
+#     3. Neither the name of Django nor the names of its contributors may be used
+#        to endorse or promote products derived from this software without
+#        specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Backports of new test features from Django 1.4 (dev).
+This can go away once we're using Django 1.4.
+"""
+
+from django.conf import settings
+from django.conf import UserSettingsHolder
+from django.utils.functional import wraps
+from django.dispatch import Signal
+
+setting_changed = Signal(providing_args=["setting", "value"])
+
+class OverrideSettingsHolder(UserSettingsHolder):
+    """
+    A custom setting holder that sends a signal upon change.
+    """
+    def __setattr__(self, name, value):
+        UserSettingsHolder.__setattr__(self, name, value)
+        setting_changed.send(sender=name, setting=name, value=value)
+
+class override_settings(object):
+    """
+    Acts as either a decorator, or a context manager. If it's a decorator it
+    takes a function and returns a wrapped function. If it's a contextmanager
+    it's used with the ``with`` statement. In either event entering/exiting
+    are called before and after, respectively, the function/block is executed.
+    """
+    def __init__(self, **kwargs):
+        self.options = kwargs
+        self.wrapped = settings._wrapped
+
+    def __enter__(self):
+        self.enable()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disable()
+
+    def __call__(self, test_func):
+        from django.test import TestCase
+        if isinstance(test_func, type) and issubclass(test_func, TestCase):
+            class inner(test_func):
+                def _pre_setup(innerself):
+                    self.enable()
+                    super(inner, innerself)._pre_setup()
+                def _post_teardown(innerself):
+                    super(inner, innerself)._post_teardown()
+                    self.disable()
+        else:
+            @wraps(test_func)
+            def inner(*args, **kwargs):
+                with self:
+                    return test_func(*args, **kwargs)
+        return inner
+
+    def enable(self):
+        override = OverrideSettingsHolder(settings._wrapped)
+        for key, new_value in self.options.items():
+            setattr(override, key, new_value)
+        settings._wrapped = override
+
+    def disable(self):
+        settings._wrapped = self.wrapped
+
+
+from django.test import testcases
+class TestCase(testcases.TestCase):
+
+    def settings(self, **kwargs):
+        """
+        A context manager that temporarily sets a setting and reverts
+        back to the original value when exiting the context.
+        """
+        return override_settings(**kwargs)
