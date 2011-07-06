bc4f5bbd71523cb75a9398180915d743ec9c2e23
Paul Winkler <slinkp@gmail.com>
2011-07-06 12:28:56 -0400

    Add a template tag for quick display of schema or place type icons; and style them.

--- ebpub/ebpub/db/templatetags/mapping.py
+++ ebpub/ebpub/db/templatetags/mapping.py
@@ -17,41 +17,19 @@
 #
 
 from django import template
-from django.conf import settings
-
-def tile_url_list():
-    return settings.TILE_URL_LIST
 
 register = template.Library()
 
-@register.inclusion_tag("etc/mapping_includes.html")
-def activate_mapping():
-    return {
-        "city": settings.SHORT_NAME,
-        "tile_url_list": tile_url_list()
-    }
-
-MAX_MARKER_RADIUS = 18
-MIN_MARKER_RADIUS = 3
-
-def _get_marker_radius(normalized_value):
-    """
-    Assumes `normalized_value` is in the range 0.0 and 1.0
-    """
-    return int(round(((MAX_MARKER_RADIUS - MIN_MARKER_RADIUS) * normalized_value) + MIN_MARKER_RADIUS))
 
 @register.simple_tag
-def get_marker_radius(normalized_value):
-    return unicode(_get_marker_radius(normalized_value))
-
-@register.simple_tag
-def get_marker_url(normalized_value):
+def map_icon_img(obj):
     """
-    Returns a URL to a marker who's size is based on a normalized (i.e., between
-    0.0 and 1.0) value.
+    Returns an image tag with a URL for a map icon for the given
+    object, which may be a db.Schema, or streets.PlaceType.
     """
-    radius = _get_marker_radius(normalized_value)
-    if hasattr(settings, 'MARKER_URL_BASE'):
-        return settings.MARKER_URL_BASE + 'marker_%d.png' % radius
-    else:
-        return '/images/mapmarkers/bubble/marker_%d.png' % radius
+    url = getattr(obj, 'map_icon_url', '') or ''
+    url = url.strip()
+    if url:
+        alt = '%s icon' % (obj.name or obj.slug)
+        return '<img class="schema-icon" src="%s" alt="%s" />' % (url, alt)
+    return ''
