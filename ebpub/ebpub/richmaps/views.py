8cdccf0473623a8594498399f6098f35db986727
Paul Winkler <slinkp@gmail.com>
2011-07-07 15:59:24 -0400

    Map popups: Add some cache  headers.

--- ebpub/ebpub/richmaps/views.py
+++ ebpub/ebpub/richmaps/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django import template
 from django.template.loader import select_template
+from django.utils.cache import patch_response_headers
 from ebpub.utils.view_utils import eb_render
 from ebpub.db.models import NewsItem
 from ebpub.streets.models import Place
@@ -15,39 +16,40 @@ def bigmap(request):
 #        'breadcrumbs': breadcrumbs.home({}),
     })
 
+
 def item_popup(request, item_id):
     """
     Given a list of newsitems, return a list of lists
     of the form [newsitem_id, popup_html, schema_name]
     """
-    try: 
+    try:
         item_id = int(item_id)
         ni = NewsItem.objects.get(id=item_id)
     except:
         return HttpResponse(status=404)
-    
+
     schema = ni.schema
-    # This goes faster if ni_list is sorted by schema.
     template_list = ['richmaps/newsitem_popup_%s.html' % schema.slug,
                      'richmaps/newsitem_popup.html',
                      ]
     current_template = select_template(template_list)
     html = current_template.render(template.Context({'newsitem': ni, 'schema': schema, }))
-    
-    return HttpResponse(html)
-    
+    response = HttpResponse(html)
+    patch_response_headers(response, cache_timeout=3600)
+    return response
+
 def place_popup(request, place_id):
     try: 
         place_id = int(place_id)
         place = Place.objects.get(id=place_id)
     except:
         return HttpResponse(status=404)
-    
-    # This goes faster if ni_list is sorted by schema.
+
     template_list = ['richmaps/place_popup_%s.html' % place.place_type.slug,
                      'richmaps/place_popup.html',
                      ]
     current_template = select_template(template_list)
     html = current_template.render(template.Context({'place': place, 'place_type': place.place_type, }))
-    
-    return HttpResponse(html)
+    response = HttpResponse(html)
+    patch_response_headers(response, cache_timeout=3600)
+    return response
