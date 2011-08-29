2019be1a06308645df566bae4b7b30c7b0de8b7b
Paul Winkler <slinkp@gmail.com>
2011-08-29 15:15:21 -0400

    Fix import_blocks to change default bounds to ogr, for compatibility with previous commit db2b2c7acd438

--- ebpub/ebpub/streets/blockimport/tiger/import_blocks.py
+++ ebpub/ebpub/streets/blockimport/tiger/import_blocks.py
@@ -98,7 +98,7 @@ class TigerImporter(BlockImporter):
     If `filter_city` is passed, we will skip features which don't have
     at least one of left_city or right_city matching the string.
 
-    If `filter_bounds` is passed, it will be treated as a geometry
+    If `filter_bounds` is passed, it will be treated as an OGR geometry
     to use for filtering out features outside the geometry.
 
     `edges_shp` contains all the feature edges (i.e. street segment
@@ -146,6 +146,8 @@ class TigerImporter(BlockImporter):
             values = dict(zip(fields, map(feature.get, fields)))
             places[fips] = values
         self.filter_city = filter_city and filter_city.upper() or None
+        if hasattr(filter_bounds, 'ogr'):
+            filter_bounds = filter_bounds.ogr
         self.filter_bounds = filter_bounds
         self.tlids_with_blocks = set()
 
