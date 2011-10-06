805e11041a18c4c5ad3a44ce9d54e9c701a08d77
Paul Winkler <slinkp@gmail.com>
2011-10-05 20:56:03 -0400

    Update OpenLayers to 2.11 final. Closes #250

--- ebpub/ebpub/settings_default.py
+++ ebpub/ebpub/settings_default.py
@@ -243,8 +243,8 @@ MAP_SCALES = [614400, 307200, 153600, 76800, 38400, 19200, 9600, 4800, 2400, 120
 
 # It's important that it be named exactly OpenLayers.js,
 # see http://trac.osgeo.org/openlayers/ticket/2982
-OPENLAYERS_URL = '/scripts/OpenLayers-2.11-rc2/OpenLayers.js'
-OPENLAYERS_IMG_PATH = '/scripts/OpenLayers-2.11-rc2/img/'
+OPENLAYERS_URL = '/scripts/OpenLayers-2.11/OpenLayers.js'
+OPENLAYERS_IMG_PATH = '/scripts/OpenLayers-2.11/img/'
 
 # For compatibility with django-olwidget
 OL_API = OPENLAYERS_URL
