919ae91edf15b0dfae7b11fb042079fed65c594c
Paul Winkler <slinkp@gmail.com>
2011-10-10 13:14:00 -0400

    Add a wsgi script and more metadata to openblock custom app paster template

--- obadmin/obadmin/skel.py
+++ obadmin/obadmin/skel.py
@@ -28,19 +28,38 @@ def _random_string(length=12):
     return result
 
 class OpenblockTemplate(templates.Template):
+    """
+    A template for the ``paster create`` command that
+    bootstraps a custom OpenBlock Django app as described in
+    the OpenBlock `custom app` docs.
+    """
     required_templates = []
     use_cheetah = False
     summary = "Basic OpenBlock project template"
     _template_dir = 'project_templates/openblock'
-    
+
     vars = [
         var('password_salt',
             'Salt used to hash passwords',
             default=_random_string()),
         var('reset_salt',
-            'Salt used to hash password resets', 
+            'Salt used to hash password resets',
             default=_random_string()),
         var('staff_cookie_val',
-            'Secret cookie value used to identify staff', 
-            default=_random_string())
+            'Secret cookie value used to identify staff',
+            default=_random_string()),
+        var('description',
+            'project description',
+            default='A Django app that provides a custom OpenBlock site'),
+        var('author',
+            'Your name for the package metadata',
+            ),
+        var('author_email',
+            'Your email for the package metadata',
+            ),
+        var('license',
+            'License',
+            default='GPLv3',
+            ),
+
     ]
