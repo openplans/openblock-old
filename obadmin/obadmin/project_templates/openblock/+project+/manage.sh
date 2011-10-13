c991ab84d945c3ca1f6d98a9989a8b4d6fd73a2d
Paul Winkler <slinkp@gmail.com>
2011-10-13 11:49:12 -0400

    manage.sh now does a better job finding a virtualenv, should work anywhere

--- obadmin/obadmin/project_templates/openblock/+project+/manage.sh
+++ obadmin/obadmin/project_templates/openblock/+project+/manage.sh
@@ -19,21 +19,8 @@
 
 
 # A tiny wrapper around manage.py that makes sure your
-# virtualenv is set up.  Symlink this into the root of the virtual env
-# and run it from there.
-
-# Find the current directory where the symlink lives.
-HERE=`(cd "${0%/*}" 2>/dev/null; echo "$PWD")`
-if [ "$VIRTUAL_ENV" != "$HERE" ]; then
-    if [ -f $HERE/bin/activate ]; then
-	source $HERE/bin/activate
-    else
-	echo "Can't find your virtualenv root."  1>&2
-	echo $0 should be a symbolic link in the root of your virtualenv. 1>&2
-	echo Check that. 1>&2
-	exit 1
-    fi
-fi
+# virtualenv is set up.
+# Should work even if you run it via a symlink.
 
 # locate the real location of this script, no matter how many symlinks there are
 ORIG_LINK="$0"
@@ -43,6 +30,55 @@ while test -n "$TMP"; do
     TMP=`readlink "$ORIG_LINK"`
 done
 
-MANAGE_PY=`dirname $ORIG_LINK`/manage.py
+HERE=`dirname $ORIG_LINK`
+export HERE=`(cd "$HERE" 2>/dev/null; echo "$PWD"/)`
+
+activator () 
+{ 
+    if [ "$VIRTUAL_ENV" != "" ]; then
+        return;
+    fi;
+
+    ACTIVATOR_STARTDIR=$PWD;
+    cd $HERE;
+    unset ACTIVATOR_SCRIPT;
+
+    while [ true ]; do
+        if [ $PWD == '/' ]; then
+            break;
+        else
+            if [ -f ./bin/activate ]; then
+                ACTIVATOR_SCRIPT=$PWD/bin/activate;
+                break;
+            else
+                if [ -f activate ]; then
+                    ACTIVATOR_SCRIPT=$PWD/activate;
+                    break;
+                else
+                    cd ..;
+                fi;
+            fi;
+        fi;
+    done;
+    if [ -f "$ACTIVATOR_SCRIPT" ]; then
+        source $ACTIVATOR_SCRIPT;
+        if [ -d "$VIRTUAL_ENV" ]; then
+            echo VIRTUAL_ENV is now $VIRTUAL_ENV;
+        fi;
+        cd $ACTIVATOR_STARTDIR;
+        unset ACTIVATOR_SCRIPT ACTIVATOR_STARTDIR;
+        return 0;
+    else
+        echo No activate script found.;
+        cd $ACTIVATOR_STARTDIR;
+        unset ACTIVATOR_SCRIPT ACTIVATOR_STARTDIR;
+        return 1;
+    fi
+}
+
+activator
+
+
+MANAGE_PY=$HERE/manage.py
 $MANAGE_PY $@
 
