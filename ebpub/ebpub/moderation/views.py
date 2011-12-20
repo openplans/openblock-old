4e151db28849e46f4a90f2fe8d1298be5884be4d
Paul Winkler <slinkp@gmail.com>
2011-12-20 17:16:06 -0500

    Add count of existing flags.

--- ebpub/ebpub/moderation/views.py
+++ ebpub/ebpub/moderation/views.py
@@ -17,6 +17,8 @@
 #
 
 from .forms import NewsItemFlagForm
+from .models import NewsItemFlag
+from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_protect
 from ebpub.db.models import NewsItem
@@ -24,15 +26,24 @@ from ebpub.utils.view_utils import eb_render
 
 @csrf_protect
 def newsitem_flag(request, newsitem_id):
-    get_object_or_404(NewsItem.objects.by_request(request).values('id'), id=newsitem_id)
+    ni = get_object_or_404(NewsItem.objects.by_request(request), id=newsitem_id)
+    flags = NewsItemFlag.objects.filter(news_item=ni).exclude(state='approved')
+    flag_counts = flags.values('reason').annotate(count=Count('id')).order_by()
+
+    context = {
+        'flag_counts': flag_counts,
+        'news_item_id': newsitem_id,
+        }
     if request.method == 'POST':
         form = NewsItemFlagForm(request.POST)
+        context['form'] = form
         if form.is_valid():
             form.save()
-            return eb_render(request, 'moderation/flagged.html', {})
+            return eb_render(request, 'moderation/flagged.html', context)
 
     else:
         form = NewsItemFlagForm()
+        context['form'] = form
 
     # Initial values are easier to set here since we have access to the request,
     # and the Form doesn't.
@@ -40,8 +51,6 @@ def newsitem_flag(request, newsitem_id):
     if not request.user.is_anonymous():
         form.fields['user'].initial = request.user.email
 
-    context = {
-        'news_item_id': newsitem_id,
-        'form': form,
-        }
+    context['form'] = form
+
     return eb_render(request, 'moderation/flag_form.html', context)
