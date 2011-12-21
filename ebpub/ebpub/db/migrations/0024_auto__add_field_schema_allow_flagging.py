5f62903f823baa701dc6c477d87acd76fc4c8581
Paul Winkler <slinkp@gmail.com>
2011-12-20 23:01:41 -0500

    Fix dependency order of migrations

--- ebpub/ebpub/db/migrations/0024_auto__add_field_schema_allow_flagging.py
+++ ebpub/ebpub/db/migrations/0024_auto__add_field_schema_allow_flagging.py
@@ -6,8 +6,13 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        # This one blows up if we run it after adding this field.
+        ('obdemo', '0007_add_meetup_and_neighbornews'),
+        )
+
     def forwards(self, orm):
-        
+
         # Adding field 'Schema.allow_flagging'
         db.add_column('db_schema', 'allow_flagging', self.gf('django.db.models.fields.BooleanField')(default=False), keep_default=False)
 
