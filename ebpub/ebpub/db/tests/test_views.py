ace4ec521b3234af7b90f7e566b24d5beca2b648
Paul Winkler <slinkp@gmail.com>
2012-03-08 11:20:46 -0500

    REmove obsolete test

--- ebpub/ebpub/db/tests/test_views.py
+++ ebpub/ebpub/db/tests/test_views.py
@@ -262,16 +262,6 @@ class TestSchemaFilterView(BaseTestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], 'http://testserver/whee/')
 
-    def test_filter__charting_disallowed_redirect(self):
-        # could make a different fixture, but, meh.
-        crime = models.Schema.objects.get(slug='crime')
-        crime.allow_charting = False
-        crime.save()
-        url = filter_reverse('crime', [('by-foo', 'bar')])
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 301)
-        self.assertEqual(response['location'], 'http://testserver/crime/')
-
     def test_filter__bad_params(self):
         url = filter_reverse('crime', [('by-foo', 'bar')])
         url = url.replace(urllib.quote('='), 'X')
