33bc0d500962bfcfef0a97a7e16434a85ceabaa7
Paul Winkler <slinkp@gmail.com>
2012-03-23 16:16:28 -0400

    Remove totally unused class

--- ebpub/ebpub/neighbornews/models.py
+++ ebpub/ebpub/neighbornews/models.py
@@ -33,26 +33,3 @@ class NewsItemCreator(models.Model):
     class Meta:
         unique_together = (('news_item', 'user'),)
         ordering = ('news_item',)
-
-
-class FeaturedLookupManager(models.Manager):
-
-    def get_by_natural_key(self, slug):
-        return self.get(lookup__slug=slug)
-
-    def featured_lookups_for(self, newsitem, sf_name):
-        """
-        Return a list of the Lookups that are featured and that the
-        newsitem has.
-        """
-        # This uses several queries, not very efficient...
-        from ebpub.db.models import SchemaField
-        sf = SchemaField.objects.get(schema__id=newsitem.schema_id, name=sf_name)
-        # Yet another manual decode of the comma-separated value,
-        # refs #265
-        if sf.is_many_to_many_lookup():
-            ni_lookup_ids = [int(i) for i in newsitem.attributes[sf_name].split(',')]
-        else:
-            ni_lookup_ids = [newsitem.attributes[sf_name]]
-        featured = self.filter(lookup__id__in=ni_lookup_ids).select_related()
-        return [obj.lookup for obj in featured]
