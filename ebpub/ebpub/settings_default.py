827103bb2625685523352d9c18fc1de30f70fbf1
Paul Winkler <slinkp@gmail.com>
2012-04-02 12:12:07 -0400

    Fix google maps copyright popup issue by forcing a specific maps API version.

--- ebpub/ebpub/settings_default.py
+++ ebpub/ebpub/settings_default.py
@@ -272,6 +272,12 @@ required_settings.append('MAP_BASELAYER_TYPE')
 
 # If you set MAP_BASELAYER_TYPE='google.*', you must also set GOOGLE_API_KEY.
 GOOGLE_API_KEY=''
+
+# Use a specific version of google maps API to work around
+# copyright alert bug:
+# http://trac.osgeo.org/openlayers/ticket/2984
+GOOGLE_API='http://maps.google.com/maps/api/js?v=3.6&sensor=false'
+
 # If you set MAP_BASELAYER_TYPE='yahoo', you must also set YAHOO_APP_ID.
 YAHOO_APP_ID=''
 # If you want MAP_BASELAYER_TYPE='cloudmade.*', you must also set CLOUDMADE_API_KEY.
